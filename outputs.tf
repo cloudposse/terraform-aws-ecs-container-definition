d7f3878df25c23dd1c4311fd8a348e3101b490e2
Sarkis Varozian <svarozian@gmail.com>
2018-06-01 08:48:56 -0700

    add description for output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "container_definitions" {
+  description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition."
   value = "${jsonencode(local.container_definitions)}"
 }
