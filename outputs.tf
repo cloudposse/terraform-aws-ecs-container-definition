436a75be9a84c02d5659e422bb2a91f634171f43
Sarkis Varozian <svarozian@gmail.com>
2018-06-05 21:44:17 -0700

    convert boolean false and then remove double quotes of ints

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "container_definitions" {
   description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition."
-  value       = "${jsonencode(local.container_definitions)}"
+  value       = "${replace(replace(jsonencode(local.container_definitions), "/\"0\"/", "false"), "/\"([0-9]+\\.?[0-9]*)\"/", "$1")}"
 }
