b0e6046e25bcb6435790143a4ca16b74551bb43f
fernandosilvacornejo <fernandosilvacornejo@users.noreply.github.com>
2018-07-27 15:58:39 -0400

    Adjust output regexp to preserve string type of environment values

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,6 @@ output "json" {
   # The following hack is required to overcome TF automatic type conversions which lead to issues with the resulting json types.
   # Conversion happens by using the built-in `replace` function in this order:
   #  - Convert `""`, `{}`, `[]`, and `[""]` to `null`
-  #  - Convert `"true"` and `"false"` to `true` and `false`
-  #  - Convert quoted numbers (e.g. `"123"`) to `123`.
-  value = "${replace(replace(replace(jsonencode(local.container_definitions), "/(\\[\\]|\\[\"\"\\]|\"\"|{})/", "null"), "/\"(true|false)\"/", "$1"), "/\"([0-9]+\\.?[0-9]*)\"/", "$1")}"
+  #  - Remove quotes from quoted numbers (e.g. `"123"`), `"true"` and `"false"`, except inside the environment's "value" key
+  value = "${replace(replace(jsonencode(local.container_definitions), "/(\\[\\]|\\[\"\"\\]|\"\"|{})/", "null"), "/(\"[^v][[:alpha:]]+\":)\"([0-9]+\\.?[0-9]*|true|false)\"/", "$1$2")}"
 }
