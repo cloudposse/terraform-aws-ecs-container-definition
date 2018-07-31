4bd3a8e3a68a94ca30fad3b96b76307197a7cfcd
fernandosilvacornejo <fernandosilvacornejo@users.noreply.github.com>
2018-07-31 19:29:57 -0400

    Add test case for string, bool and int environment variables

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,8 @@ output "json" {
   # The following hack is required to overcome TF automatic type conversions which lead to issues with the resulting json types.
   # Conversion happens by using the built-in `replace` function in this order:
   #  - Convert `""`, `{}`, `[]`, and `[""]` to `null`
-  #  - Remove quotes from quoted numbers (e.g. `"123"`), `"true"` and `"false"`, except inside the environment's "value" key
+  #  - Convert `"true"` and `"false"` to `true` and `false`
+  #  - Convert quoted numbers (e.g. `"123"`) to `123`.
+  # Environment variables are kept as strings.
   value = "${replace(replace(jsonencode(local.container_definitions), "/(\\[\\]|\\[\"\"\\]|\"\"|{})/", "null"), "/(\"[^v][[:alpha:]]+\":)\"([0-9]+\\.?[0-9]*|true|false)\"/", "$1$2")}"
 }
