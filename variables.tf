7e50ff4c50b6c5d0ef9f932dc1616554e39b61af
Andriy Knysh <aknysh@users.noreply.github.com>
2018-10-04 15:22:48 -0400

    Fix example. Update README (#13)

--- variables.tf
+++ variables.tf
@@ -1,23 +1,26 @@
 variable "container_name" {
-  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)."
+  type        = "string"
+  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
 }
 
 variable "container_image" {
-  description = "The image used to start the container. Images in the Docker Hub registry available by default."
+  type        = "string"
+  description = "The image used to start the container. Images in the Docker Hub registry available by default"
 }
 
 variable "container_memory" {
-  description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value."
+  description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
   default     = 256
 }
 
 variable "container_memory_reservation" {
-  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit."
+  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
   default     = 128
 }
 
 variable "port_mappings" {
-  description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort."
+  type        = "list"
+  description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
 
   default = [{
     "containerPort" = 80
@@ -27,52 +30,61 @@ variable "port_mappings" {
 }
 
 variable "healthcheck" {
+  type        = "map"
   description = "A map containing command (string), interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy, and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
   default     = {}
 }
 
 variable "container_cpu" {
-  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value."
+  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
   default     = 256
 }
 
 variable "essential" {
-  description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value."
+  type        = "string"
+  description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value"
   default     = "true"
 }
 
 variable "entrypoint" {
-  description = "The entry point that is passed to the container."
+  type        = "list"
+  description = "The entry point that is passed to the container"
   default     = [""]
 }
 
 variable "command" {
-  description = "The command that is passed to the container."
+  type        = "list"
+  description = "The command that is passed to the container"
   default     = [""]
 }
 
 variable "working_directory" {
-  description = "The working directory to run commands inside the container."
+  type        = "string"
+  description = "The working directory to run commands inside the container"
   default     = ""
 }
 
 variable "environment" {
-  description = "The environment variables to pas to the container. This is a list of maps."
+  type        = "list"
+  description = "The environment variables to pas to the container. This is a list of maps"
   default     = []
 }
 
 variable "readonly_root_filesystem" {
-  description = "Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value."
+  type        = "string"
+  description = "Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value"
   default     = "false"
 }
 
 variable "log_driver" {
-  description = "The log driver to use for the container. If using Fargate launch type, only supported value is awslogs."
+  type        = "string"
+  description = "The log driver to use for the container. If using Fargate launch type, only supported value is awslogs"
   default     = "awslogs"
 }
 
 variable "log_options" {
-  description = "The configuration options to send to the log_driver."
+  type        = "map"
+  description = "The configuration options to send to the `log_driver`"
 
   default = {
     "awslogs-region" = "us-west-2"
