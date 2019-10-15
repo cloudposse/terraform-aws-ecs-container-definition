a0e8da09c0b1ca0050374f60ec025183f138d421
Mark Keisler <mkeisler@enova.com>
2019-10-15 17:05:42 -0500

    terraform 0.12 syntax and simplification
    Enforce proper variable types, remove the need for regex replacements of
    empty values to null.

--- main.tf
+++ main.tf
@@ -1,39 +1,32 @@
 # Environment variables are composed into the container definition at output generation time. See outputs.tf for more information.
 locals {
   container_definition = {
-    name                   = "${var.container_name}"
-    image                  = "${var.container_image}"
-    memory                 = "memory_sentinel_value"
-    memoryReservation      = "memory_reservation_sentinel_value"
-    cpu                    = "cpu_sentinel_value"
-    essential              = "${var.essential}"
-    entryPoint             = "${var.entrypoint}"
-    command                = "${var.command}"
-    workingDirectory       = "${var.working_directory}"
-    readonlyRootFilesystem = "${var.readonly_root_filesystem}"
-    mountPoints            = "${var.mount_points}"
-    dnsServers             = "${var.dns_servers}"
-    ulimits                = "${var.ulimits}"
-    repositoryCredentials  = "${var.repository_credentials}"
-    links                  = "${var.links}"
-    volumesFrom            = "${var.volumes_from}"
-    user                   = "${var.user}"
-    dependsOn              = "${var.container_depends_on}"
-    stopTimeout            = "stop_timeout_sentinel_value"
-
-    portMappings = "${var.port_mappings}"
-
-    healthCheck = "${var.healthcheck}"
-
+    name                   = var.container_name
+    image                  = var.container_image
+    memory                 = var.container_cpu > 0 ? var.container_cpu : null
+    memoryReservation      = var.container_memory_reservation > 0 ? var.container_memory_reservation : null
+    cpu                    = var.container_cpu > 0 ? var.container_cpu : null
+    essential              = var.essential
+    entryPoint             = var.entrypoint
+    command                = var.command
+    workingDirectory       = var.working_directory
+    readonlyRootFilesystem = var.readonly_root_filesystem
+    mountPoints            = var.mount_points
+    dnsServers             = var.dns_servers
+    ulimits                = var.ulimits
+    repositoryCredentials  = var.repository_credentials
+    links                  = var.links
+    volumesFrom            = var.volumes_from
+    user                   = var.user
+    dependsOn              = var.container_depends_on
+    stopTimeout            = var.stop_timeout > 0 ? var.stop_timeout : null
+    portMappings           = var.port_mappings
+    healthCheck            = var.healthcheck
     logConfiguration = {
-      logDriver = "${var.log_driver}"
-      options   = "${var.log_options}"
+      logDriver = var.log_driver
+      options   = var.log_options
     }
-
-    environment = "environment_sentinel_value"
-    secrets     = "secrets_sentinel_value"
+    environment = var.environment
+    secrets     = var.secrets
   }
-
-  environment = "${var.environment}"
-  secrets     = "${var.secrets}"
 }
--- variables.tf
+++ variables.tf
@@ -1,111 +1,128 @@
 variable "container_name" {
-  type        = "string"
+  type        = string
   description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
 }
 
 variable "container_image" {
-  type        = "string"
+  type        = string
   description = "The image used to start the container. Images in the Docker Hub registry available by default"
 }
 
 variable "container_memory" {
   description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
+  type        = number
   default     = 256
 }
 
 variable "container_memory_reservation" {
   description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
+  type        = number
   default     = 128
 }
 
 variable "port_mappings" {
-  type        = "list"
+  type = list(
+    object(
+      {
+        containerPort = number
+        hostPort      = number
+        protocol      = string
+      }
+    )
+  )
   description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
 
-  default = [{
-    "containerPort" = 80
-    "hostPort"      = 80
-    "protocol"      = "tcp"
-  }]
+  default = [
+    {
+      "containerPort" = 80
+      "hostPort"      = 80
+      "protocol"      = "tcp"
+    },
+  ]
 }
 
 variable "healthcheck" {
-  type        = "map"
+  type        = map(string)
   description = "A map containing command (string), interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy, and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
-  default     = {}
+  default     = null
 }
 
 variable "container_cpu" {
   description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  type        = number
   default     = 256
 }
 
 variable "essential" {
-  type        = "string"
+  type        = bool
   description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason. Due to how Terraform type casts booleans in json it is required to double quote this value"
-  default     = "true"
+  default     = true
 }
 
 variable "entrypoint" {
-  type        = "list"
+  type        = list(string)
   description = "The entry point that is passed to the container"
-  default     = [""]
+  default     = null
 }
 
 variable "command" {
-  type        = "list"
+  type        = list(string)
   description = "The command that is passed to the container"
-  default     = [""]
+  default     = null
 }
 
 variable "working_directory" {
-  type        = "string"
+  type        = string
   description = "The working directory to run commands inside the container"
-  default     = ""
+  default     = null
 }
 
 variable "environment" {
-  type        = "list"
+  type        = list(map(string))
   description = "The environment variables to pass to the container. This is a list of maps"
-  default     = []
+  default     = null
 }
 
 variable "secrets" {
-  type        = "list"
+  type        = list(string)
   description = "The secrets to pass to the container. This is a list of maps"
-  default     = []
+  default     = null
 }
 
 variable "readonly_root_filesystem" {
-  type        = "string"
-  description = "Determines whether a container is given read-only access to its root filesystem. Due to how Terraform type casts booleans in json it is required to double quote this value"
-  default     = "false"
+  type        = bool
+  description = "Determines whether a container is given read-only access to its root filesystem."
+  default     = false
 }
 
 variable "log_driver" {
-  type        = "string"
+  type        = string
   description = "The log driver to use for the container. If using Fargate launch type, only supported value is awslogs"
   default     = "awslogs"
 }
 
 variable "log_options" {
-  type        = "map"
+  type        = map(string)
   description = "The configuration options to send to the `log_driver`"
 
   default = {
-    "awslogs-region" = "us-west-2"
-
-    "awslogs-group" = "default"
-
+    "awslogs-region"        = "us-west-2"
+    "awslogs-group"         = "default"
     "awslogs-stream-prefix" = "default"
   }
 }
 
 variable "mount_points" {
-  type        = "list"
+  type = list(
+    object(
+      {
+        containerPath = string
+        sourceVolume  = string
+      }
+    )
+  )
   description = "Container mount points. This is a list of maps, where each map should contain a `containerPath` and `sourceVolume`"
-  default     = []
-
+  default     = null
   #default     = [
   #  {
   #    containerPath  = "/tmp"
@@ -115,47 +132,49 @@ variable "mount_points" {
 }
 
 variable "dns_servers" {
-  type        = "list"
+  type        = list(string)
   description = "Container DNS servers. This is a list of strings specifying the IP addresses of the DNS servers."
-  default     = []
+  default     = null
 }
 
 variable "ulimits" {
-  type        = "list"
+  type        = list(string)
   description = "Container ulimit settings. This is a list of maps, where each map should contain \"name\", \"hardLimit\" and \"softLimit\""
-  default     = []
+  default     = null
 }
 
 variable "repository_credentials" {
-  type        = "map"
+  type        = map(string)
   description = "Container repository credentials; required when using a private repo.  This map currently supports a single key; \"credentialsParameter\", which should be the ARN of a Secrets Manager's secret holding the credentials"
-  default     = {}
+  default     = null
 }
 
 variable "volumes_from" {
-  type        = "list"
+  type        = list(string)
   description = "A list of VolumesFrom maps which contain \"sourceContainer\" (name of the container that has the volumes to mount) and \"readOnly\" (whether the container can write to the volume)."
-  default     = []
+  default     = null
 }
 
 variable "links" {
-  type        = "list"
+  type        = list(string)
   description = "List of container names this container can communicate with without port mappings."
-  default     = []
+  default     = null
 }
 
 variable "user" {
   description = "The user to run as inside the container. Can be any of these formats: user, user:group, uid, uid:gid, user:gid, uid:group"
-  default     = ""
+  type        = string
+  default     = null
 }
 
 variable "container_depends_on" {
-  type        = "list"
+  type        = list(string)
   description = "The dependencies defined for container startup and shutdown. A container can contain multiple dependencies. When a dependency is defined for container startup, for container shutdown it is reversed"
-  default     = []
+  default     = null
 }
 
 variable "stop_timeout" {
   description = "Timeout in seconds between sending SIGTERM and SIGKILL to container"
+  type        = number
   default     = 30
 }
