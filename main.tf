987481aa5097e430d4236302686f39ac20779563
andrewmackett <andrew@yoyowallet.com>
2019-10-22 22:21:32 +0100

    Add privileged parameter (#38)

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ locals {
     user                   = "${var.user}"
     dependsOn              = "${var.container_depends_on}"
     stopTimeout            = "stop_timeout_sentinel_value"
+    privileged             = "${var.privileged}"
 
     portMappings = "${var.port_mappings}"
 
