3daa07e0260a85e8e5d63938b3a615b7ff572937
nitro <nitrocode@users.noreply.github.com>
2020-06-14 16:32:12 -0400

    Remove default port mapping as it is optional (#77)
    Fixes https://github.com/cloudposse/terraform-aws-ecs-container-definition/issues/74
    
    Co-authored-by: Matt Gowie <gowie.matt@gmail.com>

--- variables.tf
+++ variables.tf
@@ -29,13 +29,7 @@ variable "port_mappings" {
 
   description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
 
-  default = [
-    {
-      containerPort = 80
-      hostPort      = 80
-      protocol      = "tcp"
-    }
-  ]
+  default = []
 }
 
 # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
