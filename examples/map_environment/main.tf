8dba46b486daa94a6181fcc17c30827edd0564d3
nitrocode <nitrocode@users.noreply.github.com>
2020-06-21 07:01:20 -0400

    Add map_environment test

---
+++ examples/map_environment/main.tf
@@ -0,0 +1,17 @@
+module "container" {
+  source          = "../../"
+  container_name  = "name"
+  container_image = "cloudposse/geodesic"
+
+  map_environment = {
+    "string_var"        = "I am a string"
+    "true_boolean_var"  = true
+    "false_boolean_var" = false
+    "integer_var"       = 42
+  }
+}
+
+output "json" {
+  description = "Container definition in JSON format"
+  value       = module.container.json
+}
