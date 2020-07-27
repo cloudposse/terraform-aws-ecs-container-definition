6b6b588f7dc5bf53086d2820913ca5e111d04473
nitro <nitrocode@users.noreply.github.com>
2020-07-27 18:24:55 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "json" {
-  description = "JSON encoded list of container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = "[${local.json_map}]"
+output "json_map_encoded_list" { 
+  description = "JSON string encoded list of container definitions for use with other terraform resources such as aws_ecs_task_definition" 
+  value       = "[${local.json_map}]" 
+ } 
+ 
+output "json_map_encoded" { 
+  description = "JSON string encoded container definitions for use with other terraform resources such as aws_ecs_task_definition" 
+  value       = local.json_map 
 }
 
-output "json_map" {
-  description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = local.json_map
+output "json_map_object" { 
+  description = "JSON map encoded container definition" 
+  value       = jsondecode(local.json_map)
 }
