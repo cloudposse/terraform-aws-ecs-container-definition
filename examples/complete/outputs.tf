b698e23af0c234d1dd9ccefcfe38639f400e9e48
nitro <nitrocode@users.noreply.github.com>
2020-07-28 13:40:44 -0400

    Rename JSON Outputs  (#89)
    * Update outputs.tf
    
    * Update examples_complete_test.go
    
    * Updated README.md
    
    * Update outputs.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Executed 'terraform fmt'
    
    * Update outputs.tf
    
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,9 +1,14 @@
-output "json" {
+output "json_map_encoded_list" {
   description = "JSON encoded list of container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = module.container.json
+  value       = module.container.json_map_encoded_list
 }
 
-output "json_map" {
+output "json_map_encoded" {
   description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
-  value       = module.container.json_map
+  value       = module.container.json_map_encoded
+}
+
+output "json_map_object" {
+  description = "JSON map encoded container definition"
+  value       = module.container.json_map_object
 }
--- examples/multiple_definitions/main.tf
+++ examples/multiple_definitions/main.tf
@@ -38,15 +38,15 @@ module "second_container" {
 
 output "first_container_json" {
   description = "Container definition in JSON format"
-  value       = module.first_container.json
+  value       = module.first_container.json_map_encoded_list
 }
 
 output "second_container_json" {
   description = "Container definition in JSON format"
-  value       = module.second_container.json
+  value       = module.second_container.json_map_encoded_list
 }
 
 resource "aws_ecs_task_definition" "task" {
   family                = "foo"
-  container_definitions = "[${module.first_container.json_map},${module.second_container.json_map}]"
+  container_definitions = "[${module.first_container.json_map_encoded},${module.second_container.json_map_encoded}]"
 }
