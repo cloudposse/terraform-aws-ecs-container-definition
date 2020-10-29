b0f2c2f1d7e972f5d8c7d8c38e05f3f5e1f80d67
PePe Amengual <jose.amengual@gmail.com>
2020-10-29 16:36:22 -0700

    Upgrading to support 0.13

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,9 +1,6 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
+    local    = ">= 1.2"
   }
 }
