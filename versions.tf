37e31a9e8e09837b8b71066e887099b0c6ad6769
karvounis <ekarvounis@protonmail.com>
2020-12-22 09:59:20 +0200

    pin local provider

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
-    local = ">= 1.2"
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.2"
+    }
   }
 }
