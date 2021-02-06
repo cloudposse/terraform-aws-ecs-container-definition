f61a53b7aab609a64f85977f423c1a15dbe5079f
David Vasandani <david@vasandani.me>
2021-02-06 15:55:03 -0800

    feat: add map_secrets

--- variables.tf
+++ variables.tf
@@ -105,6 +105,12 @@ variable "map_environment" {
   default     = null
 }
 
+variable "map_secrets" {
+  type        = map(string)
+  description = "The secrets variables to pass to the container. This is a map of string: {key: value}. map_secrets overrides secrets"
+  default     = null
+}
+
 # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_EnvironmentFile.html
 variable "environment_files" {
   type = list(object({
