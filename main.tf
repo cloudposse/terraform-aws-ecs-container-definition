09b3b8264383efe49191559500ea3abb16008873
Igor Rodionov <goruha@gmail.com>
2023-06-07 15:04:04 +0300

    Update main.tf
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ locals {
       valueFrom = v
     }
   ] : null
-  
   log_configuration_without_null = var.log_configuration == null ? null : {
     for k, v in var.log_configuration :
     k => v
