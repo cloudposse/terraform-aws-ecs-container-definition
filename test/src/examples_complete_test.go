b937dce4eb3f7b25536d20fdffb5bd3487113f38
Igor Rodionov <goruha@gmail.com>
2023-06-07 19:26:47 +0300

    Pin terratest version

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -30,7 +30,7 @@ func TestExamplesComplete(t *testing.T) {
 	jsonMap := terraform.OutputRequired(t, terraformOptions, "json_map_encoded")
 
 	// Verify we're getting back the outputs we expect
-	var jsonObject map[any]interface{}
+	var jsonObject map[string]interface{}
 	err := json.Unmarshal([]byte(jsonMap), &jsonObject)
 	assert.NoError(t, err)
 
--- test/src/go.mod
+++ test/src/go.mod
@@ -1,8 +1,8 @@
 module github.com/cloudposse/terraform-aws-ecs-container-definition
 
-go 1.14
+go 1.17
 
 require (
-	github.com/gruntwork-io/terratest v0.30.23
-	github.com/stretchr/testify v1.6.1
+	github.com/gruntwork-io/terratest v0.43.0
+	github.com/stretchr/testify v1.8.1
 )
