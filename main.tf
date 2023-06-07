2f099d78e3c117d4543abc4031591a9dd2d80cb7
Igor Rodionov <goruha@gmail.com>
2023-06-07 16:41:03 +0300

    Fix conflict

--- main.tf
+++ main.tf
@@ -70,9 +70,16 @@ locals {
 
   container_definition_without_null = {
     for k, v in local.container_definition :
+      k => v
+      if v != null
+  }
+
+  container_definition_override_without_null = {
+    for k, v in var.container_definition :
     k => v
     if v != null
   }
-  final_container_definition = merge(local.container_definition_without_null, var.container_definition)
+  
+  final_container_definition = merge(local.container_definition_without_null, local.container_definition_override_without_null)
   json_map                   = jsonencode(local.final_container_definition)
 }
